2a250473fd29c491736cd74e1a656c9e130f6127
Marten Mathias Jaani <marten.mathias.jaani@ut.ee>
2025-05-22 22:59:37 +0300

    changed api endpoints

--- apiparingud.js
+++ apiparingud.js
@@ -100,7 +100,7 @@ async function handleFormSubmit(event) {
 
 async function listiraamatud() {
     try {
-        const responseData = await getDataAsJson("http://localhost:5000/raamatud/");
+        const responseData = await getDataAsJson("https://praks12docker-angdhhbccza0fdec.northeurope-01.azurewebsites.net/raamatud/");
 
         const resultElement = document.getElementById("raamatud_result");
 
@@ -108,8 +108,8 @@ async function listiraamatud() {
 
         for (var raamat of responseData.raamatud) {
             raamat = raamat.split('.')[0];
-            resultElement.innerHTML += '<a href="http://localhost:5000/raamatud/'+raamat+'"  download="'+raamat+'.txt" >' +raamat+".txt</a> " +
-            '<a href="#" onclick="deleteObject(\'http://localhost:5000/raamatud/'+raamat+'\')" > [kustuta]</a>' +
+            resultElement.innerHTML += '<a href="https://praks12docker-angdhhbccza0fdec.northeurope-01.azurewebsites.net/raamatud/'+raamat+'"  download="'+raamat+'.txt" >' +raamat+".txt</a> " +
+            '<a href="#" onclick="deleteObject(\'https://praks12docker-angdhhbccza0fdec.northeurope-01.azurewebsites.net/raamatud/'+raamat+'\')" > [kustuta]</a>' +
             "<br />";
         }
     } catch (error) {
